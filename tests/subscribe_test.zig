2454b9f9bef190874f58f425fc62b8f296493c33
Lukas Lalinsky <llalinsky@live365.com>
2025-08-27 19:32:16 +0200

    Safer way to collect message

--- tests/subscribe_test.zig
+++ tests/subscribe_test.zig
@@ -15,15 +15,11 @@ test "subscribeSync smoke test" {
     try conn.publish("test", "Hello world!");
     try conn.flush();
 
-    const result = sub.nextMsg(1000);
-    defer if (result) |msg| msg.deinit();
+    const msg = sub.nextMsg(1000) orelse return error.Timeout;
+    defer msg.deinit();
 
-    if (result) |msg| {
-        try std.testing.expectEqualStrings("test", msg.subject);
-        try std.testing.expectEqualStrings("Hello world!", msg.data);
-    } else {
-        try std.testing.expect(false);
-    }
+    try std.testing.expectEqualStrings("test", msg.subject);
+    try std.testing.expectEqualStrings("Hello world!", msg.data);
 }
 
 test "queueSubscribeSync smoke test" {
@@ -36,59 +32,79 @@ test "queueSubscribeSync smoke test" {
     try conn.publish("test", "Hello world!");
     try conn.flush();
 
-    const result = sub.nextMsg(1000);
-    defer if (result) |msg| msg.deinit();
+    const msg = sub.nextMsg(1000) orelse return error.Timeout;
+    defer msg.deinit();
 
-    if (result) |msg| {
-        try std.testing.expectEqualStrings("test", msg.subject);
-        try std.testing.expectEqualStrings("Hello world!", msg.data);
-    } else {
-        try std.testing.expect(false);
+    try std.testing.expectEqualStrings("test", msg.subject);
+    try std.testing.expectEqualStrings("Hello world!", msg.data);
+}
+
+const MessageCollector = struct {
+    result: ?*Message = null,
+    mutex: std.Thread.Mutex = .{},
+    cond: std.Thread.Condition = .{},
+
+    pub fn deinit(self: *@This()) void {
+        if (self.result) |msg| msg.deinit();
     }
-}
 
-fn collectMsg(msg: *Message, result: *?*Message) !void {
-    result.* = msg;
-}
+    pub fn processMsg(msg: *Message, self: *@This()) !void {
+        self.mutex.lock();
+        defer self.mutex.unlock();
+
+        self.result = msg;
+        self.cond.broadcast();
+    }
+
+    pub fn timedWait(self: *@This(), timeout_ms: u64) !*Message {
+        self.mutex.lock();
+        defer self.mutex.unlock();
+
+        const timeout_ns = timeout_ms * std.time.ns_per_ms;
+        var timer = std.time.Timer.start() catch unreachable;
+        while (self.result == null) {
+            const elapsed_ns = timer.read();
+            if (elapsed_ns >= timeout_ns) {
+                return error.Timeout;
+            }
+            try self.cond.timedWait(&self.mutex, timeout_ns - elapsed_ns);
+        }
+        return self.result.?;
+    }
+};
 
 test "subscribe smoke test" {
     var conn = try utils.createDefaultConnection();
     defer utils.closeConnection(conn);
 
-    var result: ?*Message = null;
-    defer if (result) |msg| msg.deinit();
+    var collector: MessageCollector = .{};
+    defer collector.deinit();
 
-    const sub = try conn.subscribe("test", collectMsg, .{&result});
+    const sub = try conn.subscribe("test", MessageCollector.processMsg, .{&collector});
     defer sub.deinit();
 
     try conn.publish("test", "Hello world!");
     try conn.flush();
 
-    if (result) |msg| {
-        try std.testing.expectEqualStrings("test", msg.subject);
-        try std.testing.expectEqualStrings("Hello world!", msg.data);
-    } else {
-        try std.testing.expect(false);
-    }
+    const msg = try collector.timedWait(1000);
+    try std.testing.expectEqualStrings("test", msg.subject);
+    try std.testing.expectEqualStrings("Hello world!", msg.data);
 }
 
 test "queueSubscribe smoke test" {
     var conn = try utils.createDefaultConnection();
     defer utils.closeConnection(conn);
 
-    var result: ?*Message = null;
-    defer if (result) |msg| msg.deinit();
+    var collector: MessageCollector = .{};
+    defer collector.deinit();
 
-    const sub = try conn.queueSubscribe("test", "workers", collectMsg, .{&result});
+    const sub = try conn.queueSubscribe("test", "workers", MessageCollector.processMsg, .{&collector});
     defer sub.deinit();
 
     try conn.publish("test", "Hello world!");
     try conn.flush();
 
-    if (result) |msg| {
-        try std.testing.expectEqualStrings("test", msg.subject);
-        try std.testing.expectEqualStrings("Hello world!", msg.data);
-    } else {
-        try std.testing.expect(false);
-    }
+    const msg = try collector.timedWait(1000);
+    try std.testing.expectEqualStrings("test", msg.subject);
+    try std.testing.expectEqualStrings("Hello world!", msg.data);
 }
